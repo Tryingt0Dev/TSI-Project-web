0829599a51bf10c7cd9ac13d9ee9e37c4dbbda5d
TryingT0Dev <matias.rivasi@usm.cl>
2025-12-01 03:21:32 -0300

    alumnos seeder fix

--- database/factories/AlumnoFactory.php
+++ database/factories/AlumnoFactory.php
@@ -12,12 +12,43 @@ class AlumnoFactory extends Factory
     public function definition()
     {
         return [
-            'rut_alumno'        => strtoupper($this->faker->bothify('########-?')),
-            'nombre_alumno'     => $this->faker->firstName(),
-            'apellido_alumno'   => $this->faker->lastName(),
-            'fecha_registro'    => now(),
+            'rut_alumno'       => $this->generateRealRut(),
+            'nombre_alumno'    => $this->faker->firstName(),
+            'apellido_alumno'  => $this->faker->lastName(),
+            'fecha_registro'   => now(),
             'atrasos'          => $this->faker->numberBetween(0, 8),
-            'permiso_prestamo'  => $this->faker->boolean(90),
+            'permiso_prestamo' => $this->faker->boolean(90),
         ];
+    }
+
+    /**
+     * Genera un RUT realista con DV válido.
+     */
+    private function generateRealRut()
+    {
+        // Rango realista (alumnos jóvenes): 8–10 dígitos
+        // Ejemplo: 11.123.456-K
+        $numero = $this->faker->numberBetween(7_000_000, 26_000_000);
+
+        $dv = $this->calculateDv($numero);
+
+        return "{$numero}-{$dv}";
+    }
+
+    /**
+     * Calcula dígito verificador (DV) real para un RUT 
+     */
+    private function calculateDv($rut)
+    {
+        $s = 1;
+        $m = 0;
+
+        while ($rut > 0) {
+            $s = ($s + ($rut % 10) * (9 - $m % 6)) % 11;
+            $rut = intdiv($rut, 10);
+            $m++;
+        }
+
+        return $s ? $s - 1 : 'K';
     }
 }
