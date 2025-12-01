161d4e01b4dab45c6b999a17434fbb0c7fb7afa9
TryingT0Dev <matias.rivasi@usm.cl>
2025-12-01 03:24:54 -0300

    user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,7 +18,7 @@ class UserSeeder extends Seeder
             'name' => 'Admin',
             'apellido' => 'User',
             'rut' => '12345678-9',
-            'email' => 'admin@coffeandcatt.cl',
+            'email' => 'admin2@coffeandcatt.cl',
             'password' => Hash::make('password'),
             'rol' => 0, // 0: admin, 1: bibliotecario
             //'fecha_registro' => now()->subDays(25)
